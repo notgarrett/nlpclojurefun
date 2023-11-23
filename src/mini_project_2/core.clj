aeec67a0540b0c6a9cd57710b885cd70b521ac32
Garrett Giles <garrettrgiles@gmail.com>
2023-11-23 01:22:51 -0500

    :(

--- src/mini_project_2/core.clj
+++ src/mini_project_2/core.clj
@@ -1,19 +1,24 @@
 (ns mini-project-2.core
-  {:clj-kondo/lint-as 'clojure.core/let}
   (:gen-class)
   (:require
    [opennlp.nlp :refer :all]
    [opennlp.tools.filters :as filters]
-   [opennlp.treebank :refer :all]))
+   [opennlp.treebank :refer :all]
+   [clojure.pprint :refer :all]))
 
 (defrecord Profile
            [profile-name nouns verbs])
 
-(def use-string "long ass string")
+(def use-string (slurp "src/mini_project_2/text.txt"))
 
 (def tokenize (make-tokenizer "models/en-token.bin"))
 (def pos-tag (make-pos-tagger "models/en-pos-maxent.bin"))
 (def name-find (make-name-finder "models/en-ner-person.bin"))
+
+(declare alpha-filter)
+
+; Regex filter to remove commas and periods and other symbols.
+(filters/pos-filter alpha-filter #"[^.,]+")
 
 (defn parse-to-record
   [string]
@@ -23,4 +28,11 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (parse-to-record use-string))
+  (let [record (parse-to-record use-string)]
+    (pprint (frequencies (concat (.nouns record)
+                                 (.verbs record))))
+    ; (def word-count (count (concat (.nouns record) (.verbs record))
+    ))
+
+
+
